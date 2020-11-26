9e55f18be52785e43bb9adffb7f3d457d242c2ec
HERB Geoffroy <geoffroy.herb@hotmail.fr>
2020-11-26 11:10:01 +0100

    use log for track request

--- src/MasterClass.WebApi/Middlewares/TrackRequestContextMiddleware.cs
+++ src/MasterClass.WebApi/Middlewares/TrackRequestContextMiddleware.cs
@@ -1,21 +1,24 @@
 using System.Threading.Tasks;
 using MasterClass.WebApi.Context;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace MasterClass.WebApi.Middlewares
 {
     public class TrackRequestContextMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<TrackRequestContextMiddleware> _logger;
 
-        public TrackRequestContextMiddleware(RequestDelegate next)
+        public TrackRequestContextMiddleware(RequestDelegate next, ILogger<TrackRequestContextMiddleware> logger)
         {
             _next = next;
+            _logger = logger;
         }
 
         public async Task InvokeAsync(HttpContext context, IApplicationRequestContext requestContext)
         {
-            context.Response.Headers.Add("X-Guid", requestContext.Id.ToString());
+            _logger.LogInformation($"X-Guid : {requestContext.Id}");
             await _next(context);
         }
     }
